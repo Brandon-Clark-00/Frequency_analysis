2445074744095401f7e2cadd74db8772687da4bb
Brandon Clark <B.Y.Clark@dundee.ac.uk>
2023-06-08 18:39:48 +0100

    Adding characters to map, including basic validation. Printing map in order descending

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Frequency_analysis
 {
@@ -26,10 +28,15 @@ namespace Frequency_analysis
                 caseSensitive = false;
             }
 
-            PrintList(ParseString(input));
+
+            Dictionary<char, int> freqMap = ParseString(input);
+            PrintMap(freqMap);
             
         }
-        static List<string> ParseString(string input)
+
+
+
+      /*  static List<string> ParseString(string input)
         {
             return new List<string>(input.Split(' '));
         }
@@ -40,6 +47,45 @@ namespace Frequency_analysis
             {
                 Console.WriteLine(item);
             }
+        }*/
+
+
+        static Dictionary<char,int> ParseString(string input)
+        {
+            var map = new Dictionary<char, int>();
+            
+            foreach (var item in input)
+            {
+                int value;
+                Match m = Regex.Match(item.ToString(), @"\s|\r|\n|\t");
+                if (m.Success)
+                {
+                    //Console.WriteLine("Whitespace char");
+                    continue;
+                }
+                if (map.ContainsKey(item))
+                {
+                    map[item] += 1;
+                    //Console.WriteLine(item + ":" + map[item]);
+                }
+                else
+                {
+                    map.Add(item, 1);
+                    //Console.WriteLine(item + ":" + map[item]);
+                }
+            }
+
+            return map;
+        }
+        static void PrintMap(Dictionary<char,int> toPrint)
+        {
+            int totalChars = toPrint.Sum(x => x.Value);
+            Console.WriteLine("Total characters: " + totalChars);
+            foreach (KeyValuePair<char, int> pair in toPrint.OrderByDescending(i => i.Value))
+            {
+                Console.WriteLine(pair.Key + ":" + pair.Value);
+            }
+
         }
     }
 }
